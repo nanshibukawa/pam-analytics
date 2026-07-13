29f76009e7e4a5162964df257fde12bf3cf4280e
nanshibukawa <nanshibukawa@gmail.com>
2026-07-13 09:46:04 -0300

    chore: fix with ruff formater

--- src/ingestion/client.py
+++ src/ingestion/client.py
@@ -1,4 +1,3 @@
-
 import requests
 from src.utils.logging_config import setup_logging
 from src.ingestion.sidra_mapping import BASE_URL_API_SIDRA
@@ -7,28 +6,27 @@ from requests.adapters import HTTPAdapter
 
 logger = setup_logging()
 
+
 class SidraClient:
-    
     def __init__(self, timeout: int = 60, max_retries: int = 5):
         self.timeout = timeout
         self.session = requests.Session()
 
         retries = Retry(
             total=max_retries,
-            backoff_factor = 1, # wait 1s, 2s, 4s, 8s, 16s between retries
-            status_forcelist=[500, 501, 503, 504], # Erros derivados do servidor
-            raise_on_status=False # Se todas as tentativas falharem, não lança exceção, permitindo tratamento manual.
+            backoff_factor=1,  # wait 1s, 2s, 4s, 8s, 16s between retries
+            status_forcelist=[500, 501, 503, 504],  # Erros derivados do servidor
+            raise_on_status=False,  # Se todas as tentativas falharem, não lança exceção, permitindo tratamento manual.
         )
 
-        self.session.mount('http://', HTTPAdapter(max_retries=retries))  # type: ignore
-        
+        self.session.mount("http://", HTTPAdapter(max_retries=retries))  # type: ignore
 
     def fetch_raw_data(self, query_path: str) -> list[dict]:
         """
         Faz a chamada GET na API e retorna a lista JSON de dados.
 
         Args:
-            query_path (str): Path da API contendo todas as variáveis, 
+            query_path (str): Path da API contendo todas as variáveis,
             unidades, periodicidade, culturas e filtros territoriais.
 
         Returns:
@@ -47,7 +45,6 @@ class SidraClient:
             response.raise_for_status()
             return response.json()
 
-        
         except requests.exceptions.Timeout:
             logger.error(f"Timeout na requisição GET para {url}")
             raise Exception(f"Timeout na requisição GET para {url}")
@@ -55,6 +52,3 @@ class SidraClient:
         except requests.exceptions.RequestException as e:
             logger.error(f"Erro HTTP ao fazer GET para {url}: {e}")
             raise Exception(f"Erro HTTP ao fazer GET para {url}: {e}")
-
-
- 
--- src/ingestion/pipeline.py
+++ src/ingestion/pipeline.py
@@ -1,6 +1,5 @@
 """Pipeline de Ingestão e Tratamento de dados agrícolas da API SIDRA."""
 
-
 import json
 
 from pathlib import Path
@@ -19,6 +18,7 @@ from src.ingestion.sidra_mapping import (
 
 logger = setup_logging()
 
+
 class IngestionPipeline:
     """Orquestrador que gerencia o download dos dados brutos e o processamento final."""
 
@@ -27,7 +27,7 @@ class IngestionPipeline:
         self.base_dir = base_dir
         self.raw_dir = base_dir / "data" / "raw"
         self.processed_dir = base_dir / "data" / "processed"
-        
+
         # Garante a criação física das pastas locais
         self.raw_dir.mkdir(parents=True, exist_ok=True)
         self.processed_dir.mkdir(parents=True, exist_ok=True)
@@ -38,7 +38,7 @@ class IngestionPipeline:
         period = SidraPeriod.P2010_2024.value
         variables = ",".join(v.value for v in SidraVariables)
         location = SidraLocality.PARANA.value
-        
+
         return f"t/{table}/p/{period}/v/{variables}/n6/{location}/c782/{crop_code}"
 
     def download_all_raw_data(self):
@@ -46,19 +46,19 @@ class IngestionPipeline:
         for crop in SidraCrops:
             crop_name = crop.name.lower()
             crop_code = crop.value
-            
+
             logger.info(f"Iniciando download dos dados brutos de: {crop_name}")
             query_path = self._build_query_path(crop_code)
-            
+
             try:
                 # Baixa os dados usando o cliente
                 raw_data = self.client.fetch_raw_data(query_path)
-                
+
                 # Salva o arquivo JSON bruto no disco
                 file_path = self.raw_dir / f"pam_parana_{crop_name}_raw.json"
                 with open(file_path, "w", encoding="utf-8") as f:
                     json.dump(raw_data, f, ensure_ascii=False, indent=4)
-                    
+
                 logger.info(f"Dados brutos de {crop_name} salvos em: {file_path}")
             except Exception as e:
                 logger.error(f"Erro no download da cultura {crop_name}: {e}")
@@ -73,13 +73,13 @@ class IngestionPipeline:
 
 if __name__ == "__main__":
     import sys
-    
+
     # Resolve a raiz do projeto (sobe 3 níveis a partir de src/ingestion/pipeline.py)
     base_path = Path(__file__).resolve().parent.parent.parent
     sys.path.append(str(base_path))
-    
+
     setup_logging()
-    
+
     sidra_client = SidraClient()
     pipeline = IngestionPipeline(client=sidra_client, base_dir=base_path)
     pipeline.run()
